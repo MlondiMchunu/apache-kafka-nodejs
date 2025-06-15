ae0a184eac755708623e21dde12c80f9e4379de4
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-06-15 02:46:57 +0200

    create a topic called myUsers with 2 partitions and use kafkajs to connect to kafka broker

--- topics.js
+++ topics.js
@@ -1,2 +1,32 @@
-const {Kafka} = require("kafkajs")
+const { Kafka } = require("kafkajs")
 
+
+run();
+
+async function run() {
+    try {
+        const kafka = new Kafka({
+            "clientId": "myapp",
+            "brokers": ["192.18.7.179:9092"]
+        })
+
+        const admin = kafka.admin();
+        console.log("connecting...")
+        await admin.connect()
+        console.log("Connected!")
+
+        await admin.createTopics({
+            "topics": [{
+                "topic": "myUsers",
+                "numPartitions": 2
+            }]
+        })
+        console.log("Created succesfully")
+        await admin.disconnect();
+    }catch(error){
+        console.error(`Something wrong happened ${error}`)
+    }
+    finally{
+        process.exit(o)
+    }
+}
